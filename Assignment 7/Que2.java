a5cbbdc568e871c6918299824cede6a467651473
Kajal-Rajput-05 <161593122+Kajal-Rajput-05@users.noreply.github.com>
2024-03-20 14:09:33 +0530

    Add files via upload

---
+++ Assignment 7/Que2.java
@@ -0,0 +1,14 @@
+
+public class Que2 {
+    public static void main(String[] args) {  
+        int [] arr1 = new int [] {1, 2, 3, 4, 5};  
+        int [] arr2 = new int [] {1, 2, 3 5}; 
+          
+        //boolean res = arrays.equal(arr1, arr2);
+            if (arr1 == arr2)
+                System.out.print("Equal");  
+            else
+                System.out.print("Not Equal");
+                        
+        }  
+    }  
